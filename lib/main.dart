1c0fa4592796ef14f4d08052a55583e393b89431
houssam menzer <houssammenzer.dz@gamil.com>
2022-11-28 18:39:43 +0100

    new branch with title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Application Nice',
+      title: 'Flutter Demo from new branch',
       theme: ThemeData(
         // This is the theme of your application.
         //

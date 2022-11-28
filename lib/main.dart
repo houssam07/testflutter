7842bc0353845569e8353b12c471b19e1159b310
houssam menzer <houssammenzer.dz@gamil.com>
2022-11-28 16:13:52 +0100

    Revert "now all and see"
    This reverts commit 9ba6af0c772252fe9d91585d555a34c7154cb5a5.

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo Application Nice',
       theme: ThemeData(
+<<<<<<< HEAD
         ///dssdsdsd
         /////sdsddsdsdsdssd
+=======
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+>>>>>>> parent of 9ba6af0 (now all and see)
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
